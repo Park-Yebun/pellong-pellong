3fefd5b1d15faf24d1b281bce096dbb8e35e864d
위세영 <wiese0630@naver.com>
2024-04-29 16:03:50 +0900

    [BE][seyoung] Add Rank entity

---
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/domain/Rank.java
@@ -0,0 +1,34 @@
+package com.c205.pellongpellong.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rankId", updatable = false)
+    private Long rankId;
+
+    @OneToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @Column(name = "sumExp", nullable = false)
+    private int sumExp;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Builder
+    public Rank(Long rankId, Member member, int sumExp, String nickname) {
+        this.rankId = rankId;
+        this.member = member;
+        this.sumExp = sumExp;
+        this.nickname = nickname;
+    }
+}
