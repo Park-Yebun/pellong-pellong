c4d1711187cf3f95067a324edc43bfa78ec23bc7
RupertJoo <rokaf0743@gmail.com>
2024-05-03 09:53:15 +0900

    [BE][Joo] fix: my mistake..

--- BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/LifeQuoteService.java
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/LifeQuoteService.java
@@ -2,14 +2,19 @@ package com.c205.pellongpellong.service;
 
 import com.c205.pellongpellong.dto.LifeQuoteDTO;
 import com.c205.pellongpellong.repository.LifeQuoteRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LifeQuoteService {
     private final LifeQuoteRepository lifeQuoteRepository;
 
-    public LifeQuoteDTO getLifeQuoteById(Long lifeQuoteId) {
-        return lifeQuoteRepository.findById(lifeQuoteId)
-                .orElseThrow(() -> new RuntimeException("Life Quote not found with id: " + lifeQuoteId));
+//    public LifeQuoteDTO getLifeQuoteById(Long lifeQuoteId) {
+    public int getLifeQuoteById(Long lifeQuoteId) {
+//        return lifeQuoteRepository.findById(lifeQuoteId).equals(lifeQuoteId)
+        return 1;
+//        return lifeQuoteRepository.findById(lifeQuoteId)
+//                .orElseThrow(() -> new RuntimeException("Life Quote not found with id: " + lifeQuoteId));
     }
 }
