4f780419a6b9ddd8dad2bcb0b8f170eec9227468
dbtywi <dbtywi@naver.com>
2024-05-07 08:53:04 +0900

    [FE][yebun] WaitingRoomPage added

--- FE_develop/src/pages/JejuPlay/WaitingRoomPage.tsx
+++ FE_develop/src/pages/JejuPlay/WaitingRoomPage.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BackButton from '../../components/BackButton'
+import {
+  Container,
+  MainTextBox,
+  Player,
+  PlayerContainer,
+  StartBtn,
+  SubTextBox,
+  UpperBox
+} from './WaitingRoomPage.styled'
 
 const WaitingRoomPage = () => {
   return (
-    <div></div>
+    <Container>
+      <UpperBox>
+        <BackButton/>
+        <SubTextBox>PLAY !!</SubTextBox>
+        <MainTextBox>인원이 모이면 퀴즈를 시작해요</MainTextBox>
+      </UpperBox>
+      <PlayerContainer>
+        <Player>
+          
+        </Player>
+        <Player></Player>
+        <Player></Player>
+        <Player></Player>
+      </PlayerContainer>
+      <StartBtn>퀴즈 시작</StartBtn>
+    </Container>
   );
 };
 
