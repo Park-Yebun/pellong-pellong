4d5938a315fabf9d783db3b7bf449d6c04c8b8c8
RupertJoo <rokaf0743@gmail.com>
2024-05-07 18:03:24 +0900

    [BE][Joo] release annotation config.CorsConfig

--- BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/config/CorsConfig.java
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/config/CorsConfig.java
@@ -1,16 +1,16 @@
-//package com.c205.pellongpellong.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/**")
-//                .allowedMethods("*")
-//                .allowedOrigins("http://client");
-//    }
-//}
+package com.c205.pellongpellong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://client");
+    }
+}
