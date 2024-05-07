ce7ac460abd810ec2ab40ff93383813722e62636
mooncheolhwan <2020vipbeat@gmail.com>
2024-05-07 16:44:14 +0900

    [BE][cheolhwan]ADD Drama API, get request only, sql Update

---
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/repository/DramaRepository.java
@@ -0,0 +1,7 @@
+package com.c205.pellongpellong.repository;
+
+import com.c205.pellongpellong.entity.Drama;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DramaRepository extends JpaRepository<Drama, Long> {
+}
