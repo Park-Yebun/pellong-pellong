5b4c11d8430da058a2e375db61850fb9592170bf
위세영 <wiese0630@naver.com>
2024-05-08 17:17:45 +0900

    [FE][seyoung] Change redirect URI

--- FE_develop/src/pages/TestPage.tsx
+++ FE_develop/src/pages/TestPage.tsx
@@ -9,25 +9,25 @@ const TestPage = () => {
 
   return (
     <div>
-      <a href="http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000&mode=login">
+      <a href="http://localhost:8080/oauth2/authorization/google?redirect_uri=https://www.saturituri.com/test&mode=login">
         <button>Google Login</button>
       </a>
-      <a href="http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000&mode=unlink">
+      <a href="http://localhost:8080/oauth2/authorization/google?redirect_uri=https://www.saturituri.com/test&mode=unlink">
         <button>Google Unlink</button>
       </a>
-      <a href="http://localhost:8080/oauth2/authorization/naver?redirect_uri=http://localhost:3000&mode=login">
+      <a href="http://localhost:8080/oauth2/authorization/naver?redirect_uri=https://www.saturituri.com/test&mode=login">
         <button>Naver Login</button>
       </a>
 
-      <a href="http://localhost:8080/oauth2/authorization/naver?redirect_uri=http://localhost:3000&mode=unlink">
+      <a href="http://localhost:8080/oauth2/authorization/naver?redirect_uri=https://www.saturituri.com/test&mode=unlink">
         <button>Naver Unlink</button>
       </a>
 
-      <a href="http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000&mode=login">
+      <a href="http://localhost:8080/oauth2/authorization/kakao?redirect_uri=https://www.saturituri.com/test&mode=login">
         <button>Kakao Login</button>
       </a>
 
-      <a href="http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000&mode=unlink">
+      <a href="http://localhost:8080/oauth2/authorization/kakao?redirect_uri=https://www.saturituri.com/test&mode=unlink">
         <button>Kakao Unlink</button>
       </a>
 
