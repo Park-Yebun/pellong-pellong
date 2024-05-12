32067996a1858c576265c5458cf03ba67f91f91e
박예분 <dbtywi@naver.com>
2024-05-12 15:22:20 +0900

    [Fix][yebun] websocket config fixed

--- BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/config/WebSocketConfig.java
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/config/WebSocketConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    // 예분 수정
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
