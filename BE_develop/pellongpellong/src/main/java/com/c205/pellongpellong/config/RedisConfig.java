306a0d6b3244800c17879ae78134ecf87bd0833f
위세영 <wiese0630@naver.com>
2024-05-13 14:42:22 +0900

    [BE][seyoung] ãã레디스 ì´초기설정

---
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.c205.pellongpellong.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<Long, String> redisTemplate() {
+        RedisTemplate<Long, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); //key 깨짐 방지
+        redisTemplate.setValueSerializer(new StringRedisSerializer());//value 깨짐 방지
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
