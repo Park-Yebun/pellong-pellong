53114ee949b8f65ae600ff62de392cbf316bd836
박예분 <dbtywi@naver.com>
2024-05-14 20:37:29 +0900

    [Fix][yebun] debug data response

--- FE_develop/src/pages/JejuPlay/WaitingRoomPage.tsx
+++ FE_develop/src/pages/JejuPlay/WaitingRoomPage.tsx
@@ -55,7 +55,7 @@ const WaitingRoomPage = () => {
       // 구독 요청
       client.subscribe("/topic/party/" + partyId, function(message){
         const response = JSON.parse(message.body)
-        // console.log("구독 요청 후 응답 데이터!!", response)
+        console.log("구독 요청 후 응답 데이터!!", response)
       });
       // 클라이언트 > 서버 메세지 보내기(참여자 추가요청)
       client.send(`/app/party/guest`, {},JSON.stringify({partyId: partyId, memberId: 40}));
