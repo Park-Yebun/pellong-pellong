c36521bb78e114b5b3324212e519825c8f393a41
RupertJoo <rokaf0743@gmail.com>
2024-05-15 23:23:22 +0900

    [BE] [Joo] FEAT the elementary redis app

--- BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/ExpService.java
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/ExpService.java
@@ -9,7 +9,6 @@ import com.c205.pellongpellong.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,7 +20,7 @@ public class ExpService {
 
     private final ExpRepository expRepository;
     private final MemberRepository memberRepository;
-    private final RedisTemplate redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
     public List<ExpDTO> getExpByMemberId(Long memberId) {
         // ExpRepository를 사용하여 memberId에 해당하는 Exp 데이터를 가져옴
         List<Exp> expList = expRepository.findByMember_MemberId(memberId);
@@ -38,7 +37,7 @@ public class ExpService {
                 .collect(Collectors.toList());
     }
 
-    public Exp addExp(long myId, String expName, int expValue) {
+    public void addExp(long myId, String expName, int expValue) {
         Member member = memberRepository.findById(myId)
                 .orElseThrow(() -> new RuntimeException("해당 memberId에 해당하는 회원이 존재하지 않습니다."));
         Exp exp = Exp.builder()
@@ -48,9 +47,8 @@ public class ExpService {
                 .expAt(LocalDateTime.now())
                 .build();
         //redis에 data 전송
-        redisTemplate.opsForZSet().add("ranking", member.getMemberId(), expValue);
-
-        return expRepository.save(exp);
+        redisTemplate.opsForZSet().incrementScore("ranking", String.valueOf(member.getMemberId()), expValue);
+        expRepository.save(exp);
     }
 
     public void addPlayersExp(List<PlayerDTO> players, String expName) {
@@ -64,7 +62,8 @@ public class ExpService {
                     .expAt(LocalDateTime.now())
                     .build();
             //redis에 data 전송
-            redisTemplate.opsForZSet().add("ranking", member.getMemberId(), player.getPlayerExp());
+//            redisTemplate.opsForZSet().add("ranking", myJson, player.getPlayerExp());
+            redisTemplate.opsForZSet().incrementScore("ranking", String.valueOf(member.getMemberId()), player.getPlayerExp());
             expRepository.save(exp);
         }
     }
--- BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/RankService.java
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/RankService.java
@@ -2,6 +2,7 @@ package com.c205.pellongpellong.service;
 
 import com.c205.pellongpellong.dto.*;
 import com.c205.pellongpellong.entity.Rank;
+import com.c205.pellongpellong.repository.MemberRepository;
 import com.c205.pellongpellong.repository.RankRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,8 +10,9 @@ import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class RankService {
 
     private final RankRepository rankRepository;
     private final RedisTemplate<String, String> redisTemplate;
+    private final MemberRepository memberRepository;
 
     public MyInfoRankDTO getMyInfoRank(long memberId){
         Rank rank = rankRepository.findById(memberId).orElseThrow(() -> new RuntimeException("User not found"));
@@ -39,6 +42,31 @@ public class RankService {
         return rankDTOs;
     }
 
+    public List<RankDTO> getAllRankInRedis() {
+        Set<ZSetOperations.TypedTuple<String>> typedTuples = redisTemplate.opsForZSet().reverseRangeWithScores("ranking", 0, -1);
+//        List<Rank> ranks = rankRepository.findAll();  // 모든 Rank 조회
+
+
+//        List<RankDTO> rankDTOs = ranks.stream()
+//                .map(rank -> rank.of(rank))
+//                .toList();
+        AtomicLong rankId = new AtomicLong(1);
+
+        assert typedTuples != null;
+        return typedTuples.stream()
+                .map(typedTuple -> {
+//                    Long memberId = Long.parseLong(Objects.requireNonNull(typedTuple.getValue()));
+                    Long memberId = Long.parseLong(Objects.requireNonNull(typedTuple.getValue()));
+                    int sumExp = Objects.requireNonNull(typedTuple.getScore()).intValue();
+                    String nickName = memberRepository.getNicknameByMemberId(memberId).orElseThrow(() -> new RuntimeException("해당 memberId에 해당하는 회원의 닉네임이 존재하지 않습니다."));
+//                    int sumExp = Integer.parseInt(Double.toString(typedTuple.getScore()));
+
+                    RankDTO rankDTO = new  RankDTO(rankId.get(), memberId, sumExp, nickName);
+                    rankId.addAndGet(1);
+                    return rankDTO;
+                }).toList();
+    }
+
     public Rank updateSumExp(long myId, int expValue) {
         Rank rank = rankRepository.findByMemberMemberId(myId)
                 .orElseThrow(() -> new RuntimeException("해당 memberId에 해당하는 회원의 Rank가 존재하지 않습니다."));
@@ -57,15 +85,15 @@ public class RankService {
 
     // 여기서부터 Redis 이용한 랭킹 시스템 메서드
     // 상위 10개의 랭킹을 가져옴, 개수는 추후 커스텀
-    public List<RankingDTO> getRankingList() {
-        String key = "ranking";
-        ZSetOperations<String, String> stringStringZSetOperations = redisTemplate.opsForZSet();
-        Set<ZSetOperations.TypedTuple<String>> typedTuples = stringStringZSetOperations.reverseRangeWithScores(key, 0L, 10L);
-        List<RankingDTO> collect = typedTuples.stream()
-                .map(typedTuple -> new RankingDTO(Long.parseLong(typedTuple.getValue()), Integer.parseInt(Double.toString(typedTuple.getScore()))))
-                .collect(Collectors.toList());
-        return collect;
-    }
+//    public List<RankingDTO> getRankingList() {
+//        String key = "ranking";
+//        ZSetOperations<String, String> stringStringZSetOperations = redisTemplate.opsForZSet();
+//        Set<ZSetOperations.TypedTuple<String>> typedTuples = stringStringZSetOperations.reverseRangeWithScores(key, 0L, 10L);
+//        List<RankingDTO> collect = typedTuples.stream()
+//                .map(typedTuple -> new RankingDTO(Long.parseLong(typedTuple.getValue()), Integer.parseInt(Double.toString(typedTuple.getScore()))))
+//                .collect(Collectors.toList());
+//        return collect;
+//    }
 
 
     public Long getRank(Long memberId) {
