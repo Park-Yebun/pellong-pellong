f8c31c3c1dedd14de6219984adf0c18abe950d6a
위세영 <wiese0630@naver.com>
2024-05-17 12:18:28 +0900

    [BE][세영] 전체 랭킹 조회 api 연결

--- FE_develop/src/pages/MyPage/RankPage.tsx
+++ FE_develop/src/pages/MyPage/RankPage.tsx
@@ -5,41 +5,29 @@ import './RankPage.css';
 import BackButton from '../../components/BackButton';
 
 interface User {
-  id: number;
-  username: string;
-  experience: number;
-  profilePicture: string;
-  rankBadge: string;
-  rank: number;
+  memberId: number;
+  nickname: string;
+  sumExp: number;
+  profileImg: string;
+  // rankBadge: string;
+  rankId: number;
 }
 
-const mockUsers: User[] = [
-    { id: 1, username: '예부리', experience: 100, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/small.png', rank: 0 },
-    { id: 2, username: '감수꽝', experience: 200, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/middle.png', rank: 0 }, 
-    { id: 3, username: '김감귤', experience: 300, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/big.png', rank: 0 }, 
-    { id: 4, username: '개성주악', experience: 400, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/sobig.png', rank: 0 },
-    { id: 5, username: '슈슈슉', experience: 500, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/brilliant.png', rank: 0 }, 
-    { id: 6, username: 'eee', experience: 420, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/small.png', rank: 0 },
-    { id: 7, username: 'ddd', experience: 250, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/middle.png', rank: 0 },
-    { id: 8, username: 'vvv', experience: 370, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/big.png', rank: 0 },
-    { id: 9, username: 'bbb', experience: 480, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/sobig.png', rank: 0 }, 
-    { id: 10, username: 'aaa', experience: 550, profilePicture:'../../assets/badge/small.png', rankBadge: '../../assets/badge/brilliant.png', rank: 0 },
-  ];
 
   const sortAndRankUsers = (users: User[]): User[] => {
-    const sortedUsers = users.sort((a, b) => b.experience - a.experience);
+    const sortedUsers = users.sort((a, b) => b.sumExp - a.sumExp);
     let rank = 1;
-    let prevExperience = sortedUsers[0].experience;
+    let prevExperience = sortedUsers[0].sumExp;
   
     console.log("=== 랭크 부여 과정 ===");
     console.log("랭크\t아이디\t유저네임\t경험치");
     
     const rankedUsers = sortedUsers.map((user, index) => {
-      if (index > 0 && user.experience !== prevExperience) {
+      if (index > 0 && user.sumExp !== prevExperience) {
         rank++;
       }
-      console.log(`${rank}\t${user.id}\t${user.username}\t${user.experience}`);
-      prevExperience = user.experience;
+      console.log(`${rank}\t${user.memberId}\t${user.nickname}\t${user.sumExp}`);
+      prevExperience = user.sumExp;
       return { ...user, rank };
     });
   
@@ -50,8 +38,24 @@ const mockUsers: User[] = [
     const [users, setUsers] = useState<User[]>([]);
   
     useEffect(() => {
-      const rankedUsers = sortAndRankUsers(mockUsers);
-      setUsers(rankedUsers);
+      console.log("페치데이터 동작!!")
+      const fetchUsers = async () => {
+        try {
+          const response = await fetch('https://www.saturituri.com/api/ranking', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+          const data: User[] = await response.json();
+          const rankedUsers = sortAndRankUsers(data);
+          setUsers(rankedUsers);
+          console.log("데이터 로드 완료", data);
+        } catch (error) {
+          console.log("데이터 로드 실패", error)
+        }
+      }
+      fetchUsers();
     }, []);
   
     return (
@@ -60,15 +64,15 @@ const mockUsers: User[] = [
         <div className='ranking-title'>랭킹 조회</div>
         <div className="user-grid">
           {users.map((user) => (
-            <div key={user.id} className="user-card">
+            <div key={user.memberId} className="user-card">
               <div className="user-info">
-                <span className="rank-number">{user.rank}</span>
+                <span className="rank-number">{user.rankId}</span>
                 <span className="rank-badge">
-                  <img src={user.rankBadge} alt="" />
+                  {/* <img src={user.rankBadge} alt="" /> */}
                 </span>
-                <img className="profile-picture" src={user.profilePicture} alt={`Profile of ${user.username}`} />
-                <p className="username">{user.username}</p>
-                <p className="experience">{user.experience}xp</p>
+                <img className="profile-picture" src={user.profileImg} alt={`Profile of ${user.nickname}`} />
+                <p className="username">{user.nickname}</p>
+                <p className="experience">{user.sumExp}xp</p>
               </div>
             </div>
           ))}
