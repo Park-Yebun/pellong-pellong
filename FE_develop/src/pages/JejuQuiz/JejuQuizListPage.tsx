452dfad3704eec0c359fdf120438c927142c6ef6
위세영 <wiese0630@naver.com>
2024-05-18 23:55:50 +0900

    [FE][세영] 유저별 진행 상황에 따라 자물쇠로 락이 걸립니다

--- FE_develop/src/pages/JejuQuiz/JejuQuizListPage.tsx
+++ FE_develop/src/pages/JejuQuiz/JejuQuizListPage.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect } from "react";
-import { Link } from 'react-router-dom';
-import useStore from '../../store';
+import React, { useEffect, useState } from "react";
 import BackButton from '../../components/BackButton';
 import LevelPath from './LevelPath'; // Import the LevelPath component
 import './JejuQuizListPage.css'; // Import the correct CSS file
 
 const LevelListPage: React.FC = () => {
-  const store = useStore();
-
-  useEffect(() => {
-    // store.loginUserInfo?.memberId 값이 변경될 때마다 콘솔에 출력
-    console.log("Member ID:", store.loginUserInfo?.memberId);
-  }, [store.loginUserInfo?.memberId]); // 의존성 배열에 store.loginUserInfo?.memberId 추가
-
 
 
 
@@ -20,7 +11,7 @@ const LevelListPage: React.FC = () => {
     <div className="quiz-list-container">
       <BackButton />
       <div className="level-list-container"> {/* 최상위 요소에 클래스 추가 */}
-        <LevelPath />
+        <LevelPath/>
       </div>
     </div>
   );
--- FE_develop/src/pages/JejuQuiz/LevelPath.tsx
+++ FE_develop/src/pages/JejuQuiz/LevelPath.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+import useStore from '../../store';
 import './LevelPath.css'; // Import the CSS file
 
 const levels = Array.from({ length: 10 }, (_, i) => i + 1);
 
 const LevelPath: React.FC = () => {
+  const store = useStore();
+  const [data, setData] = useState<number | null>(null); // 데이터 상태 추가
+
+  useEffect(() => {
+    // store.loginUserInfo?.memberId 값이 변경될 때마다 콘솔에 출력
+    console.log("Member ID:", store.loginUserInfo?.memberId);
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://www.saturituri.com/api/learning/' + store.loginUserInfo?.memberId, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await response.json();
+        // 데이터에 숫자 잘들어옴
+        console.log('데이터', data);
+        setData(data); // 데이터 상태 업데이트
+      } catch (error) {
+        console.log("데이터 로드 실패", error)
+      }
+    }
+    fetchData();
+  })
+
   return (
     <div className="level-path-container">
       <div className="level-path-header">가족</div>
@@ -16,9 +43,18 @@ const LevelPath: React.FC = () => {
                 <div className="stone"></div>
               </div>
             )}
-            <Link to={`/jeju-quiz/level/${level}`} className={`level-circle level-${level}`}>
-              {level}
-            </Link>
+            {/* 조건부 렌더링 */}
+            {data && data >= level ? ( // data가 존재하고 data가 level 이상인 경우
+              <Link to={`/jeju-quiz/level/${level}`} className={`level-circle level-${level}`}>
+                {level}
+              </Link>
+            ) : ( // 그 외의 경우
+              <div className={`level-circle level-${level}`}>
+                <div className="level-image">
+                  <img src='../../assets/badges/00blind.png' alt="얍얍" />
+                </div>
+              </div>
+            )}
           </React.Fragment>
         ))}
       </div>
