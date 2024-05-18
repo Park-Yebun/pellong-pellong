ace2d7217794e992a72d27826017290728116139
위세영 <wiese0630@naver.com>
2024-05-18 22:59:13 +0900

    [FE][세영] memberId를 찍어보자

--- FE_develop/src/pages/JejuQuiz/JejuQuizListPage.tsx
+++ FE_develop/src/pages/JejuQuiz/JejuQuizListPage.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
+import useStore from '../../store';
 import BackButton from '../../components/BackButton';
 import LevelPath from './LevelPath'; // Import the LevelPath component
 import './JejuQuizListPage.css'; // Import the correct CSS file
 
 const LevelListPage: React.FC = () => {
+  const store = useStore();
+
+  useEffect(() => {
+    // store.loginUserInfo?.memberId 값이 변경될 때마다 콘솔에 출력
+    console.log("Member ID:", store.loginUserInfo?.memberId);
+  }, [store.loginUserInfo?.memberId]); // 의존성 배열에 store.loginUserInfo?.memberId 추가
+
+
+
+
   return (
     <div className="quiz-list-container">
       <BackButton />
