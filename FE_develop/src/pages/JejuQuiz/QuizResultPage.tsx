c5287d6fca2c20e2ba6df17c621b50f9ab98dfe9
위세영 <wiese0630@naver.com>
2024-05-19 19:35:54 +0900

    [FE][세영] 기본  exp 적립 api 호출 완료

--- FE_develop/src/pages/JejuQuiz/QuizResultPage.tsx
+++ FE_develop/src/pages/JejuQuiz/QuizResultPage.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import './QuizResultPage.css'
+import useStore from '../../store';
 
 interface QuizResultPageProps {
   score: number;
@@ -8,9 +9,31 @@ interface QuizResultPageProps {
 }
 
 const QuizResultPage: React.FC = () => {
+  const store = useStore();
   const navigate = useNavigate();
   const location = useLocation();
   const { score, totalQuestions } = location.state as QuizResultPageProps;
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        console.log(store.loginUserInfo?.memberId)
+        const response = await fetch(`https://www.saturituri.com/api/exp/quiz-solving/${store.loginUserInfo?.memberId}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        console.log(response)
+        // 데이터 처리
+      } catch (error) {
+        console.error("API 호출에 실패했습니다:", error);
+      }
+    };
+
+    fetchData(); // 컴포넌트가 마운트될 때 API 호출
+  }, []);
 
   const handleGoBack = () => {
     navigate("/jeju-quiz"); // 퀴즈 목록 페이지로 이동
