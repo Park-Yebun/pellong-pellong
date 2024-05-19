890e858e8aa6a3265b07b836d946c2c762bc5152
dbtywi <dbtywi@naver.com>
2024-05-20 04:05:37 +0900

    [FE][yebun] websocket added

--- FE_develop/src/pages/JejuPlay/Speed/SpeedPlayPage.tsx
+++ FE_develop/src/pages/JejuPlay/Speed/SpeedPlayPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams, useNavigate, useLocation } from 'react-router-dom';
 import profileImg from '../../../assets/JejuPlay/profile.png'
 import useStore from '../../../store';
+import useWebsocket from '../../../contexts/useWebsocket';
 import {
     Example,
     CheckBadge,
@@ -14,60 +15,70 @@ import {
     ProfileImg,
     TimerBox,
     Description,
-    Timer
+    Timer,
+    ExampleImg,
 } from './SpeedPlayPage.styled'
 
 // 웹소켓 통신
 import SockJS from 'sockjs-client';
 import {Stomp, Frame} from '@stomp/stompjs';
-import { json } from 'stream/consumers';
+import { userInfo } from 'os';
+
+interface UserInfo {
+  guestId: number;
+  nickname: string;
+  profileImg: string;
+  memberId: number;
+}
+
+
+interface EnhancedUserInfo extends UserInfo {
+  correctCount: number;
+  alert: string;
+}
 
 const SpeedPlayPage = () => {
-const [count, setCount] = useState(10);
-const [quizList, setQuizList] = useState<any[]>([]);
-const [quiz, setQuiz] = useState<any|null>(null);
-const [userInfo, setUserInfo] = useState<any[]>([]);
-const { partyId } = useParams();
-const navigate = useNavigate();
-const store = useStore();
+  const { connected, connect, disconnect, client } = useWebsocket();
+  const [count, setCount] = useState(10);
+  const [quizList, setQuizList] = useState<any[]>([]);
+  const [quiz, setQuiz] = useState<any|null>(null);
+  const [shuffledOptions, setShuffledOptions] = useState<any[]>([]);
+  const { partyId } = useParams();
+  const navigate = useNavigate();
+  const store = useStore();
+  const location = useLocation();
+  const [isDone, setIsDone] = useState<boolean>(false);
 
- // 클라이언트 할당
- const socket = new SockJS('https://www.saturituri.com/ws');
- let client = Stomp.over(socket);
+  // 유저 관련 변수들
+  const userInfos = location.state?.userInfos;
+  const [players, setPlayers] = useState<EnhancedUserInfo[]>(userInfos.map((user: UserInfo) => ({
+    ...user,
+    correctCount: 0,
+    alert: ''
+  })));
 
- useEffect(() => {
-   // 소켓 연결
-   client.connect({}, () => {
-     console.log("웹소켓이 연결되었습니다.")
 
-     // 구독 요청
-     client.subscribe("/topic/party/" + partyId, function(message){
-       if (message.body === "유저가 퇴장했습니다.") {
-        // 클라이언트 > 서버 메세지 보내기(참여자 입장요청)
-        setTimeout(() => {
-          client.send(`/app/party/guest`, {}, JSON.stringify({partyId: partyId, memberId: store.loginUserInfo?.memberId}));
-        }, 500); // 500밀리초 후에 실행
-       } else {
-        setUserInfo(JSON.parse(message.body).guests);
-       };
-     });
-     
-   })
-   return () => {
-     // 클라이언트 > 서버 메세지 보내기(참여자 퇴장요청)
-     try{
-       client.send(`/app/party/guest/delete`, {},JSON.stringify({partyId: partyId, memberId: store.loginUserInfo?.memberId}));
-       console.log("퇴장 메세지를 전송했습니다.", store.loginUserInfo?.memberId);
-     } catch (error) {
-       console.error("메세지 전송 중 오류가 발생했습니다:", error);
-     }
+const handleClick = (option:any) => {
+  if (option === quiz.false && client) {
+    const numPartyId = Number(partyId)
+    client.publish({
+      destination: `/app/party/${numPartyId}/correct/${store.loginUserInfo?.memberId}`,
+      body: ''
+    });
+  } else if (option !== quiz.false && client) {
+    const numPartyId = Number(partyId)
+    client.publish({
+      destination: `/app/party/${numPartyId}/wrong/${store.loginUserInfo?.memberId}`,
+      body: ''
+    });
+  }
+}
 
-     client.disconnect(() => {
-       console.log("웹소켓 연결이 해제되었습니다.")
-     });
-   };
- }, [partyId]);
+useEffect(() => {
+  connect();
+}, [connect]);
 
+// 시간 제한
 useEffect(() => {
   const id = setInterval(() => {
       setCount(prevCount => prevCount > 0 ? prevCount - 1 : 0); 
@@ -75,6 +86,33 @@ useEffect(() => {
   return () => clearInterval(id);
 }, []);
 
+useEffect(() => {
+  if (connected && client) {
+    console.log("구독 요청을 보냅니다.");
+    const numPartyId = Number(partyId)
+    const subscription = client.subscribe(`/topic/party/` + numPartyId, (message) => {
+      const data = JSON.parse(message.body);
+      console.log('구독 요청 후 응답 데이터!!', data);
+
+      // 1. 정답/오답 메세지 받아올경우
+      if (data.type == 'correct' || data.type == 'wrong') {
+        setPlayers(prevPlayers => prevPlayers.map(player => {
+          console.log(player.memberId)
+          if (player.memberId === data.memberId) {
+            return {
+              ...player,
+              correctCount: data.type === 'correct' ? player.correctCount + 1 : player.correctCount,
+              alert: data.type === 'correct' ? '정답!' : '오답!'
+            };
+          }
+          return player;
+        }));
+      }
+      console.log(players)
+
+    });
+  }
+}, [connected]);
 
 useEffect(() => {
   const fetchData = async () => {
@@ -86,24 +124,31 @@ useEffect(() => {
         }
       });
       const data = await response.json();
-      setQuizList(data);
-      console.log('데이터 로드 성공')
+      if (data.length > 0) {
+        const quiz = data[Math.floor(Math.random() * data.length)];
+        setQuiz(quiz);
+      }
     } catch (error) {
-      console.log('데이터 로드 실패', error)
+      console.error('Data load failed:', error);
     }
-  }
-  fetchData()
+  };
+  fetchData();
 }, []);
 
+// 퀴즈 들어올때마다 셔플
 useEffect(() => {
-  if (quizList.length > 0) {
-    const randomIndex = Math.floor(Math.random() * 10);
-    setQuiz(quizList[randomIndex]);
-    console.log('선택된 랜덤 퀴즈:', quizList[randomIndex]);
-  } else {
-    console.log('퀴즈 리스트가 비어있습니다.');
+  if (quiz) {
+    const options = [
+    quiz.dialectText,
+    quiz.standardText,
+    quiz.false,
+    quiz.dialectImage
+  ];
+  const shuffled = options.sort(() => Math.random() - 0.5);
+  setShuffledOptions(shuffled);
   }
-}, [quizList]);
+}, [quiz]);
+
 
   return (
     <Container>
@@ -113,32 +158,23 @@ useEffect(() => {
         <Description>뜻이 다른 한 장을 <br/>
           제외한 카드를 터치하세요.</Description>
         <ExampleBox>
-            <Example>{quiz ? quiz.dialectText : 'Loading...'}</Example>
-            <Example>{quiz ? quiz.standardText : 'Loading...'}</Example>
-            <Example>{quiz ? quiz.false : 'Loading...'}</Example>
-            <Example>{quiz ? quiz.dialectImage : 'Loading...'}</Example>
+        {shuffledOptions.map((option, index) => {
+            // Assume the original image URL is still in the shuffled options
+            if (option === quiz.dialectImage) {
+              return <ExampleImg key={index} src={option} alt="Dialect" onClick={() => handleClick(option)}/>;
+            } else {
+              return <Example key={index} onClick={() => handleClick(option)} >{option}</Example>;
+            }
+          })}
         </ExampleBox>
         <PlayerBox>
-            <Player>
-                <ExpBadge></ExpBadge>
-                <ProfileImg src={profileImg}></ProfileImg>
-                <Nickname>예분</Nickname>
-            </Player>
-            <Player>
-                <ExpBadge></ExpBadge>
-                <ProfileImg src={profileImg}></ProfileImg>
-                <Nickname>세영</Nickname>
-            </Player>
-            <Player>
-                <ExpBadge></ExpBadge>
-                <ProfileImg src={profileImg}></ProfileImg>
-                <Nickname>홍찬</Nickname>
-            </Player>
-            <Player>
-                <ExpBadge></ExpBadge>
-                <ProfileImg src={profileImg}></ProfileImg>
-                <Nickname>준형</Nickname>
-            </Player>
+        {players.map((player) => (
+          <Player key={player.guestId}>
+              <ExpBadge>{player.alert}</ExpBadge>
+              <ProfileImg src={player.profileImg} alt='profile-image'></ProfileImg>
+              <Nickname>{player.nickname}</Nickname>
+          </Player>
+        ))}
         </PlayerBox>
     </Container>
   );
