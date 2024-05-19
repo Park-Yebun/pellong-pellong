3869637939486c82ae6fb79de68f69fed95734ff
RupertJoo <rokaf0743@gmail.com>
2024-05-19 20:14:01 +0900

    feat be jeju-proverb

---
+++ BE_develop/pellongpellong/src/main/java/com/c205/pellongpellong/service/JejuProverbService.java
@@ -0,0 +1,19 @@
+package com.c205.pellongpellong.service;
+
+import com.c205.pellongpellong.dto.JejuProverbDTO;
+import com.c205.pellongpellong.entity.JejuProverb;
+import com.c205.pellongpellong.repository.JejuProverbRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JejuProverbService {
+    private final JejuProverbRepository jejuProverbRepository;
+
+    public JejuProverbDTO getJejuProverb(Long jejuProverbId) {
+        JejuProverb jejuProverb = jejuProverbRepository.findById(jejuProverbId)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + jejuProverbId));
+        return new JejuProverbDTO(jejuProverb.getJejuProverbId(), jejuProverb.getPbStandard(), jejuProverb.getPbStandard());
+    }
+}
